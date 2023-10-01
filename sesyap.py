5642bf339468d59788153e6f296566a2e9a5006e
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-10-01 21:21:57 +0400

    Update sesyap.py

--- sesyap.py
+++ sesyap.py
@@ -1,10 +1,11 @@
+python
 import telebot
 from gtts import gTTS
-from io import BytesIO
+import io
 
 bot_token = "6086089724:AAELu6YRS_U0JuJMmWPyhtnBMWJ18iOnPRY"
 bot = telebot.TeleBot("6086089724:AAELu6YRS_U0JuJMmWPyhtnBMWJ18iOnPRY")
-print("Sesyap Online")
+print("Sesyap Aktif")
 
 @bot.message_handler(commands=['ses']) # /ses komutuna cevap veren fonksiyon
 def convert_to_speech(message):
@@ -15,8 +16,8 @@ def convert_to_speech(message):
         return
     
     tts = gTTS(text[0], lang='tr') # Girilen metni sese dönüştür
-    audio = BytesIO()
-    tts.save(audio)
+    audio = io.BytesIO()
+    tts.save(audio, format='mp3')
     audio.seek(0)
     bot.send_audio(message.chat.id, audio) # Ses dosyasını kullanıcıya gönder
 
